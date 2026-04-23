2d7ed33c01e2ffbf81d7357c34368c9e2ce6587d
aidan-smagh <aidansmagh@gmail.com>
2026-04-22 23:38:20 -0400

    Added a coroutine to delay enemy spawning

--- Assets/Scripts/EnemyInstantiator.cs
+++ Assets/Scripts/EnemyInstantiator.cs
@@ -1,14 +1,27 @@
 using UnityEngine;
+using System.Collections;
 
 public class EnemyInstantiator : MonoBehaviour
 {
     [SerializeField] private GameObject enemy;
     void Start()
     {
+        /*
         if (enemy == null) Debug.Log("cant find enemy");
         for (var i = 0; i < 10; i++)
         {
             Instantiate(enemy, new Vector3(i * 2.0f, 0, 0), Quaternion.identity);
         }
+        */
+        StartCoroutine(SpawnEnemiesRoutine());
+    }
+    IEnumerator SpawnEnemiesRoutine()
+    {
+        for (var i = 0; i < 10; i++)
+        {
+            yield return new WaitForSeconds(2.0f);
+            Instantiate(enemy, new Vector3(i * 2.0f, 0, 0), Quaternion.identity);
+        }
+
     }
 }
