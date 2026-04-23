dec4754e1b718353bbdef9f2d90bb6517d671da9
aidan-smagh <aidansmagh@gmail.com>
2026-04-23 00:17:09 -0400

    Fix collision bug and add preliminary game over screen

--- Assets/Scripts/EnemyInstantiator.cs
+++ Assets/Scripts/EnemyInstantiator.cs
@@ -6,13 +6,6 @@ public class EnemyInstantiator : MonoBehaviour
     [SerializeField] private GameObject enemy;
     void Start()
     {
-        /*
-        if (enemy == null) Debug.Log("cant find enemy");
-        for (var i = 0; i < 10; i++)
-        {
-            Instantiate(enemy, new Vector3(i * 2.0f, 0, 0), Quaternion.identity);
-        }
-        */
         StartCoroutine(SpawnEnemiesRoutine());
     }
     IEnumerator SpawnEnemiesRoutine()
--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -5,10 +5,18 @@ public class PlayerStats : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI playerHealthText;
     [SerializeField] private int playerHealthNum = 2;
+    [SerializeField] private GameObject gameOverScreen;
 
     void Update()
     {
         playerHealthText.text = playerHealthNum.ToString();
+        //Debug.Log(playerHealthNum);
+        
+        if (playerHealthNum <= 0)
+        {
+            gameOverScreen.SetActive(true);
+        }
+        
     }
 
     public int DecrementHealthOnHit()
@@ -19,4 +27,5 @@ public class PlayerStats : MonoBehaviour
     {
         return playerHealthNum;
     }
+
 }
